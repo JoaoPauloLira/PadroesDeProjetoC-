e790462ea37b37c6f087c321d50c72b4f777b831
João Paulo Lira da Silva <paulo.pitang@ons.org.br>
2022-05-04 07:05:57 -0300

    Comit Inicial

---
+++ Padroes de Projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Padroes_de_Projeto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
